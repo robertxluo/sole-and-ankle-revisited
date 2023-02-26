cb6a7c6b80f2221ef3a68a7424ff303441fb3666
Robert Luo <robertxluo@gmail.com>
2023-02-25 19:12:28 -0800

    Set up breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+const BREAKPOINTS = {
+  phone: 37.5,
+  tablet: 59.375,
+  laptop: 81.25,
+};
+
+export const QUERIES = {
+  phone: `(max-width: ${BREAKPOINTS.phone}rem)`,
+  tablet: `(max-width: ${BREAKPOINTS.tablet}rem)`,
+  laptop: `(max-width: ${BREAKPOINTS.laptop}rem)`,
+};
