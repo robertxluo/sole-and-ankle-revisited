97f6fffd3114bd8f21290b783959b05c77a0c97c
Robert Luo <robertxluo@gmail.com>
2023-02-26 14:24:06 -0800

    Complete fluid desktop navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -46,13 +46,13 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: auto;
 `;
 
 const Nav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {
